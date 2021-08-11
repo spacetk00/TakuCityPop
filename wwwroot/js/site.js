c9ce7798faf6faa8a40efe5e0a4825d76ffe3227
Paulo <paulorcmv@gmail.com>
2021-08-10 23:02:36 -0300

    Funcionalidades extras adicionadas

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,38 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// VALIDAÇÃO
+function checa_form(form){
+
+    var invalid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+
+    let caixa_nome = document.querySelector('.msg-nome');
+    caixa_nome.style.display = 'none';
+
+    let caixa_email = document.querySelector('.msg-email');
+    caixa_email.style.display = 'none';
+
+    if(form.nome.value==""){
+        
+        caixa_nome.innerHTML = "Campo obrigatório";
+        caixa_nome.style.display = 'block';
+        form.nome.focus();
+        return false;
+    }
+
+    if(form.email.value==""){
+        
+        caixa_email.innerHTML = "Campo obrigatório";
+        caixa_email.style.display = 'block';
+        form.email.focus();
+        return false;
+    }
+
+    if (invalid.test(form.email.value)==false){
+        caixa_email.innerHTML = "Digite um email válido";
+        caixa_email.style.display = 'block';
+        form.email.focus();
+        return false;
+    }
+}
